d15b4dfc3446540852fbe25542fb54cbf990d789
Brianna Townsed <b.townsend42@yahoo.com>
2023-06-25 12:22:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+const companyName = 'Scuber';// Code your solution in this file!
+const NewCompanyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+
